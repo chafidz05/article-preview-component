7a210f19d6aecc92564f74f69ed3767f51358ab3
chafidz05 <chafidz0505@gmail.com>
2023-07-01 18:27:53 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const shareBtn = document.querySelector(".share-btn");
+const hiddenShareBtn = document.querySelector(".hidden-share-btn");
+const shareSection = document.querySelector(".share-section");
+const shareLink = document.querySelector(".share-link");
+const socialMediaSection = document.querySelector(".social-icons");
+
+const toggleSection = () => shareSection.classList.toggle("shown");
+const removeHiddenSection = () => shareSection.classList.remove("shown");
+
+shareBtn.addEventListener("click", function() {
+  toggleSection();
+});
+hiddenShareBtn.addEventListener("click", function() {
+  toggleSection();
+});
+
+// HIDE SECTION EVENT
+document.addEventListener("click", function(e) {
+  let targetSection = shareBtn.contains(e.target);
+  let showntargetSection = hiddenShareBtn.contains(e.target);
+  let sharebleLink = shareLink.contains(e.target);
+  let socialIcons = socialMediaSection.contains(e.target);
+  if(!showntargetSection && !targetSection && !sharebleLink && !socialIcons) {
+    removeHiddenSection();
+  }
+});
